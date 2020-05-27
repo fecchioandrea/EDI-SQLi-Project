d6fa1cccd370371d1a7152bb88cd2b0a9bf61006
Andrea Fecchio <48335273+fecchioandrea@users.noreply.github.com>
2020-05-27 15:20:33 +0000

    Update fuzzer.py

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -9,10 +9,9 @@ def main():
 
     """
     args = parse_args(sys.argv[1:])
-    #num_threads = args.concurrency
 
     url = args.url
-    make_post_requests(args.data, url, codes, num_threads)
+    make_post_requests(args.param, url, codes)
 
 
 if __name__ == '__main__':
