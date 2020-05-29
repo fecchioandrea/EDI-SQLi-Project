597db05714bc2017fce3f920ccfeab0074b1b3e6
Janluke <gianlucacaronte@gmail.com>
2020-05-29 19:04:57 +0200

    refactor dumper.py

--- db_dumper/dumper.py
+++ db_dumper/dumper.py
@@ -1,9 +1,5 @@
 from query_executor import MySQLExecutor 
-
-db = MySQLExecutor('localhost','root','')
-
-schemas = {}
-
+from pprint import pprint
 
 def type_sql2py(t: str):
     t = t.lower()
@@ -17,43 +13,56 @@ def type_sql2py(t: str):
     #    return datetime
     return str
 
+def grep_schemas(db,schemas=None):
+    if schemas is None:
+        schemas = {}
+    """grep databases/tables"""
+    for schema, table in db.execute(
+                            columns=['TABLE_SCHEMA', 'TABLE_NAME'], 
+                            column_types=[str,str],
+                            schema="INFORMATION_SCHEMA",
+                            table="TABLES",
+                            raw_condition="TABLE_TYPE <> 'SYSTEM VIEW'"):
+        if schema not in schemas:
+            schemas[schema] = {}
+        schemas[schema][table] = {
+            'column_names':[], 
+            'column_types':[], 
+            'rows':[]
+            }
+    
+    return schemas
 
-#grep databases/tables
-for schema, table in db.execute(
-                        columns=['TABLE_SCHEMA', 'TABLE_NAME'], 
-                        column_types=[str,str],
-                        schema="INFORMATION_SCHEMA",
-                        table="TABLES",
-                        raw_condition="TABLE_TYPE <> 'SYSTEM VIEW'"):
-    if schema not in schemas:
-        schemas[schema] = {}
-    schemas[schema][table] = {
-        'column_names':[], 
-        'column_types':[], 
-        'rows':[]
-        }
-        
-#grep columns of each table
-for schema in schemas:
-    for table in schemas[schema]:
-        t = schemas[schema][table]
-        #grep column info
-        for name, _type in db.execute(
-                        columns=['COLUMN_NAME', 'COLUMN_TYPE'], 
-                        column_types=[str, type_sql2py],
-                        schema="INFORMATION_SCHEMA",
-                        table="COLUMNS",
-                        raw_condition=f"TABLE_NAME='{table}' and TABLE_SCHEMA='{schema}'"):
-            t['column_names'].append(name)
-            t['column_types'].append(_type)
 
-#grep rows
-for schema in schemas:
-    for table in schemas[schema]:
-        t = schemas[schema][table]
-        for row in db.execute(t['column_names'], t['column_types'], schema, table):
-            t['rows'].append(row)
+def grep_column_structures(db, schemas):
+    """grep columns of each table"""
+    for schema in schemas:
+        for table in schemas[schema]:
+            t = schemas[schema][table]
+            #grep column info
+            for name, _type in db.execute(
+                            columns=['COLUMN_NAME', 'COLUMN_TYPE'], 
+                            column_types=[str, type_sql2py],
+                            schema="INFORMATION_SCHEMA",
+                            table="COLUMNS",
+                            raw_condition=f"TABLE_NAME='{table}' and TABLE_SCHEMA='{schema}'"):
+                t['column_names'].append(name)
+                t['column_types'].append(_type)
+    return schemas
 
-from pprint import pprint
-pprint(schemas['edi'])
+def grep_rows(db, schemas):
+    for schema in schemas:
+        for table in schemas[schema]:
+            t = schemas[schema][table]
+            for row in db.execute(t['column_names'], t['column_types'], schema, table):
+                t['rows'].append(row)
+    return schemas
+
+if __name__ == "__main__":
+    db = MySQLExecutor('localhost','root','')
+    schemas = grep_schemas(db)
+    schemas = grep_column_structures(db,schemas)
+    schemas = grep_rows(db,schemas)
+
+    pprint(schemas['edi'])
 
