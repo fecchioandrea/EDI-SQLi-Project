377b8cacd34901c7965fc556dcaed7e0e796e5fe
Andrea Fecchio <48335273+fecchioandrea@users.noreply.github.com>
2020-06-02 14:53:44 +0000

    Update post_requests.py

--- src/post_requests.py
+++ src/post_requests.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 MY_URL = ""
 PARAM = {}
@@ -15,17 +16,28 @@ def download_all_post(codes):
         PARAM[user_key] = "random"
         first_try_resp = requests.post(MY_URL, PARAM, allow_redirects=False)
         nominal_size = len(first_try_resp.content)
+        
+        tot = 0;
+        found = 0;
+        
+        start = time.clock()
 
         for c in codes:
             PARAM[user_key] = c
             resp = requests.post(MY_URL, PARAM, allow_redirects=False)
             resp_length = len(resp.content)
+            tot += 1
             print("status ", resp.status_code, " --- ", resp_length, " bytes --- using string: ", c,
                   " --- RESPONSE:")
             if (resp_length != nominal_size and resp_length > 0):
                 print(resp.content, "\n\n\n\n")
+                found += 1
             else:
                 print(" - Nothing interesting... \n\n")
+        
+        end = time.clock()
+        print("\n\nSTATISTICS:\t\t", found, " interesting responses over ", tot, " tried.")
+        print("\t\tTotal time: ", "{0:.2f}".format(end-start), " seconds.")
 
 
 
